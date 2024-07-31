fa3054e9156f08d8ce9bbd4762f2282cfea7e62b
Carla <107934506+carlasmartinsr@users.noreply.github.com>
2024-07-31 22:35:48 +0300

    Update index.css

--- index.js
+++ index.js
@@ -63,15 +63,11 @@ window.onload = function () {
 const elements = document.querySelectorAll(".hover-effect");
 
 elements.forEach((element) => {
-  // Add touchstart event listener
   element.addEventListener("touchstart", () => {
-    console.log("test1");
     element.classList.add("touch-hover-effect");
   });
 
   element.addEventListener("touchend", () => {
-    // Remove touch effect
-    console.log("test2");
     element.classList.remove("touch-hover-effect");
   });
 });
