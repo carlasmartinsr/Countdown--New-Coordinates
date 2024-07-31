95243d42c02ac5a927ea4521b6c21d2ededece13
Carla <107934506+carlasmartinsr@users.noreply.github.com>
2024-07-31 16:02:28 +0300

    commit index

--- index.js
+++ index.js
@@ -7,53 +7,52 @@ const days = document.querySelector("#days");
 const hours = document.querySelector("#hours");
 const minutes = document.querySelector("#minutes");
 const seconds = document.querySelector("#seconds");
-const emoji = document.querySelector("#emoji");
+let leftDays, leftHours, leftMinutes, leftSeconds;
 
 const expirationDate = new Date("August 22, 2024 09:00:00");
 const expirationTime = expirationDate.getTime();
 
-function display(leftDays, leftHours, leftMinutes, leftSeconds, departure) {
+function display() {
   days.textContent = leftDays;
   hours.textContent = leftHours;
   minutes.textContent = leftMinutes;
   seconds.textContent = leftSeconds;
-  departure = 0;
-  if (departure <= 0) {
-    Confetti();
-  }
 }
+
 function updateCountDown() {
   const nowDate = new Date();
   const nowTime = nowDate.getTime();
   const departure = expirationTime - nowTime;
 
-  let leftDays = Math.floor(departure / (1000 * 60 * 60 * 24));
-  let leftHours = Math.floor(
+  leftDays = Math.floor(departure / (1000 * 60 * 60 * 24));
+  leftHours = Math.floor(
     (departure % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
-  let leftMinutes = Math.floor((departure % (1000 * 60 * 60)) / (1000 * 60));
-  let leftSeconds = Math.floor((departure % (1000 * 60)) / 1000);
-
-  display(leftDays, leftHours, leftMinutes, leftSeconds, departure);
+  leftMinutes = Math.floor((departure % (1000 * 60 * 60)) / (1000 * 60));
+  leftSeconds = Math.floor((departure % (1000 * 60)) / 1000);
+  leftHours = 0;
+  leftDays = 0;
+  leftMinutes = 0;
+  leftSeconds = 0;
+  display();
 }
 
-async function Confetti() {
-  await jsConfetti.addConfetti({
-    emojis: ["🌈", "⚡️", "💥", "✨", "💫", "🤍", "🚀"],
-    confettiColors: [
-      "#ff0a54",
-      "#ff477e",
-      "#ff7096",
-      "#ff85a1",
-      "#fbb1bd",
-      "#f9bec7",
-    ],
-    confettiRadius: 6,
-    confettiNumber: 150,
-    emojiSize: 22,
-  });
-  window.location.reload;
+function confetti() {
+  if (
+    leftDays === 0 &&
+    leftHours === 0 &&
+    leftMinutes === 0 &&
+    leftSeconds === 0
+  ) {
+    jsConfetti.addConfetti({
+      emojis: ["🌈", "⚡️", "💥", "✨", "💫", "🤍", "🚀"],
+      confettiRadius: 6,
+      confettiNumber: 300,
+      emojiSize: 22,
+    });
+  }
 }
 
 updateCountDown();
 setInterval(updateCountDown, 1000);
+confetti();
